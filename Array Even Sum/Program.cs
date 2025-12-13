763012878a658ba822a35fa1bf7962f2668d5ea9
IvanPasyuk <94356533+IvanPasyuk@users.noreply.github.com>
2025-12-14 00:30:54 +0300

    Creating repository
    Adding files for the lessons

---
+++ Array Even Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int arrayCount = int.Parse(Console.ReadLine());
+
+            int[] userArray = new int[arrayCount];
+            int sumEvenArrayElements = 0;
+
+            for (int i = 0; i < userArray.Length; i++)
+            {
+                int arrayNumber = i + 1;
+                Console.WriteLine("Введите {0}й элемент массива: ", arrayNumber);
+                userArray[i] = int.Parse(Console.ReadLine());
+
+                if (userArray[i] % 2 == 0)
+                {
+                    sumEvenArrayElements += userArray[i];
+                }
+            }
+
+            Console.WriteLine("\nСумма чётных чисел в массиве: ");
+            Console.WriteLine(sumEvenArrayElements);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Array Reverse Output/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int arrayCount = int.Parse(Console.ReadLine());
+
+            int[] userArray = new int[arrayCount];
+
+            for (int i = 0; i < userArray.Length; i++)
+            {
+                int arrayNumber = i + 1;
+                Console.WriteLine("Введите {0}й элемент массива: ", arrayNumber);
+                userArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nВведённый массив в обратном порядке: ");
+            for (int i = (userArray.Length - 1); i >= 0; i--)
+            {
+                Console.Write(userArray[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
